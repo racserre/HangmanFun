518d390172573ae6612247eb367d3df67d5402ce
racserre <153023119+racserre@users.noreply.github.com>
2025-01-26 18:59:44 -0700

    Add project files.

---
+++ HangmanTools.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HangmanFun
+{
+    internal class HangmanTools
+    {
+        public string GetRandomWord()
+        {
+            Random r = new Random();
+
+            string word = "";
+
+            // Dynamic array is a list because the arrays are not dynamic
+            List<string> words = new List<string>()
+            {
+                "hello",
+                "customer",
+                "synthetic",
+                "maid",
+                "stranger",
+                "offbeat",
+                "bleach",
+                "laugh"
+            };
+
+            //words.Add("hello");
+            //words.Add("customer");
+            //words.Add("synthetic");
+
+            // Get a random word
+            word = words[r.Next(words.Count)];
+
+            return word;
+        }
+
+        public bool ValidGuess(string guess, string lettersGuessed)
+        {
+            bool result = true; // Default to a valid guess
+
+            if (guess.Length != 1) // Check for only one character
+            {
+                Console.WriteLine("Sorry, the guess can only be one letter. Try again.");
+                result = false;
+
+            }
+
+            else if (!Char.IsLetter(guess[0])) // Check to make sure it is a letter
+            {
+                Console.WriteLine("Sorry, the guess needs to be a letter. Try again.");
+                result = false;
+            }
+
+            else if (lettersGuessed.Contains(guess)) // Check to see if the letter has already been guessed.
+            {
+                Console.WriteLine("Sorry, you already guessed that letter. Try again.");
+                result = false;
+            }
+
+            return result;
+        }
+
+        public string UpdateWord(string lettersGuessed, string solution)
+        {
+            string result = "";
+
+            for (int i = 0; i < solution.Length; i++)
+            {
+                if (lettersGuessed.Contains(solution[i])) // Is the letter in the list?
+                {
+                    result += solution[i];
+                }
+
+                else
+                {
+                    result += "_";
+                }
+            }
+
+            return result;
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,86 @@
+﻿using System.ComponentModel.Design;
+using HangmanFun;
+
+// Declare all variables and objects at the top
+HangmanTools ht = new HangmanTools();
+
+string solution = "";
+string guess = "";
+string lettersGuessed = "";
+int inCorrectGuesses = 0;
+
+
+bool gameOver = false;
+
+// Welcome the user to the game
+Console.WriteLine("Welcome to our Hangman Game!");
+
+
+// Get the random word for the user to guess
+Console.Write("Generating a random word");
+Thread.Sleep(300);
+Console.Write(".");
+Thread.Sleep(300);
+Console.Write(".");
+Thread.Sleep(300);
+Console.Write(".");
+
+solution = ht.GetRandomWord();
+
+Console.WriteLine();
+
+Console.WriteLine("Alright, I have your word to guess!");
+
+do
+{
+    // Get the user's guess
+    Console.WriteLine("Please enter a letter: ");
+
+    // Validate the guess
+    do
+    {
+        guess = Console.ReadLine();
+
+
+    } while (!ht.ValidGuess(guess, lettersGuessed));
+
+    // Update the list of letters guessed
+    lettersGuessed += guess;
+
+    // Check to see if the letter is in the word
+    if (solution.Contains(guess))
+    {
+        Console.WriteLine($"Yes, the letter {guess} is in the word!");
+
+
+        if (ht.UpdateWord(lettersGuessed, solution) == solution)
+        {
+            Console.WriteLine("You won!");
+            gameOver = true;
+        }
+
+        Console.WriteLine("Word: " + ht.UpdateWord(lettersGuessed, solution));
+    }
+    else // Guess was incorrect
+    {
+        Console.WriteLine($"Sorry, the word does not have the letter {guess}.");
+
+        inCorrectGuesses++;
+
+        if (inCorrectGuesses < 6)
+        {
+            Console.WriteLine($"You have {6 - inCorrectGuesses} guesses left");
+        }
+        if (inCorrectGuesses >= 6)
+        {
+            Console.WriteLine("Sorry, you lost!");
+            Console.WriteLine($"The word was: {solution}");
+            gameOver = true;
+        }
+        
+    }
+
+
+} while (!gameOver);
+
+Console.WriteLine("Thanks for playing! Please come again!");
